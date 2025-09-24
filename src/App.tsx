e5148d795f4397be9f11491a028c2f18aee39941
KoTerryHtay <luaye1990@gmail.com>
2025-09-24 10:52:19 +0630

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
 // import Hero1 from "./components/Hero-v1";
 
 export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       {/* <Hero1 /> */}
+      <NavBar />
       <Hero />
       <About />
     </main>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,31 @@
 type Props = {
   id: string;
   title: string;
-  leftIcon: React.ReactNode;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
   className: string;
   // containerClass: string;
 };
 
-export default function Button({ id, title, leftIcon, className }: Props) {
+export default function Button({
+  id,
+  title,
+  leftIcon,
+  rightIcon,
+  className,
+}: Props) {
   return (
     <button
       id={id}
       className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${className}`}
     >
       {leftIcon}
+
       <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
         <div>{title}</div>
       </span>
+
+      {rightIcon}
     </button>
   );
 }
